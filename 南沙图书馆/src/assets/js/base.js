e873c1af30bd2780ac16a77966fd2518c88c9592
pactera-338 <pactera-338>
2019-04-09 13:47:09 +0800

    添加目录

--- 南沙图书馆/src/assets/js/base.js
+++ 南沙图书馆/src/assets/js/base.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-    
+    $(function(){
+        var globalHeight = $('.header').height() + $('.page-banner').height() + $('.footer').height()
+        $('.aside').css( { minHeight : ( $(window).height() - globalHeight )+ 'px' } )
+        $(window).resize(function(){
+            $('.aside').css( { minHeight : ( $(window).height() - globalHeight )+ 'px' } )
+        })
+    })
 })
