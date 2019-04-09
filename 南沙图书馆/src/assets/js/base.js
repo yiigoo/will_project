161953b891eeb3d05f6b041cf66702fd29c09224
pactera-338 <pactera-338>
2019-04-09 15:44:31 +0800

    添加主题

--- 南沙图书馆/src/assets/js/base.js
+++ 南沙图书馆/src/assets/js/base.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
     $(function(){
         var globalHeight = $('.header').height() + $('.page-banner').height() + $('.footer').height()
-        $('.aside').css( { minHeight : ( $(window).height() - globalHeight )+ 'px' } )
-        $(window).resize(function(){
+        var mainHeight = $('.main').height()
+        if ( mainHeight > $(window).height() - globalHeight ){
+            $('.aside').css( { minHeight :mainHeight + 'px' } )
+        }else{
             $('.aside').css( { minHeight : ( $(window).height() - globalHeight )+ 'px' } )
+        }
+        
+        $(window).resize(function(){
+            var mainHeight = $('.main').height()
+            if ( mainHeight > $(window).height() - globalHeight ){
+                $('.aside').css( { minHeight :mainHeight + 'px' } )
+            }else{
+                $('.aside').css( { minHeight : ( $(window).height() - globalHeight )+ 'px' } )
+            }
         })
     })
 })
