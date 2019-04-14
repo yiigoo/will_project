46ff3f2d3335a1ef57dec2127935f10a1d32473c
yiigoo <yiigoo@163.com>
2019-04-14 11:44:59 +0800

    add swiper css , swiper js

--- 南沙图书馆/src/assets/js/base.js
+++ 南沙图书馆/src/assets/js/base.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $(function(){
+        // 设置左边导航高度
         var globalHeight = $('.header').height() + $('.page-banner').height() + $('.footer').height()
         var mainHeight = $('.main').height()
         if ( mainHeight > $(window).height() - globalHeight ){
@@ -16,5 +17,15 @@ $(document).ready(function(){
                 $('.aside').css( { minHeight : ( $(window).height() - globalHeight )+ 'px' } )
             }
         })
+
+        // 设置tabs
+        if( $(".site-tabs").length ){
+             var next = $(".site-tabs").next()
+             next.find(".panel").eq(0).show()
+             $(".site-tabs li").on("click",function(){
+                $(this).addClass("active").siblings().removeClass("active")
+                next.find(".panel").hide().eq($(this).index()).show()
+             })
+        }
     })
 })
